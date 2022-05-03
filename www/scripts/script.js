4f04ae81c9198eb6cf383a01487ffb6087a99715
Alexei Pak <lexx_1996@mail.ru>
2022-05-03 08:02:15 +0300

    аккордион

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,24 @@
-console.log('script work!');
+$(document).ready(function() {
+
+  $('.j-burger').on( 'click' , function() {
+    $('.j-menu').slideToggle();
+  });
+
+  let prevBtn;
+
+  $('.j-accordion-btn').on('click', function(){
+
+    if (prevBtn === this) {
+      $(this).next().slideToggle();
+      return;
+    }
+
+    $('.j-accordion-btn').next().slideUp();
+    $(this).next().slideDown();
+    prevBtn = this;
+
+  });
+
+
+
+});
