453a00c05833866bfaed5c1b5a72dab95fcba7f9
Alexei Pak <lexx_1996@mail.ru>
2022-05-10 10:30:49 +0300

    стили

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
   // бургер
-  $('.j-burger').on( 'click' , function() {
+  $('.j-burger').on('click', function () {
     $('.j-menu').slideToggle();
   });
 
@@ -10,7 +10,7 @@ $(document).ready(function() {
 
   let prevBtn;
 
-  $('.j-accordion-btn').on('click', function(){
+  $('.j-accordion-btn').on('click', function () {
 
     if (prevBtn === this) {
       $(this).next().slideToggle();
@@ -25,7 +25,7 @@ $(document).ready(function() {
 
   // табы
 
-  $('.j-tabs-link').on('click', function(event) {
+  $('.j-tabs-link').on('click', function (event) {
     event.preventDefault();
 
     let index = $(this).index('.j-tabs-link');
@@ -36,7 +36,38 @@ $(document).ready(function() {
     $('.j-contacts-item').removeClass('active');
     $('.j-contacts-item').eq(index).addClass('active');
 
-  })
+  });
 
+  // filter
+
+  $('.j-works-link').on('click', function(event) {
+    event.preventDefault();
+
+    let dataFilter = $(this).data('filter');
+
+    $('.j-works-link').removeClass('active');
+    $(this).addClass('active');
+
+    if (dataFilter === 'all') {
+      $('.j-works-item').show();
+      return;
+    }
+
+    $('.j-works-item').each(function() {
+      let dataType = $(this).data('type');
+
+      if (dataType === dataFilter) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+    });
+
+  });
+
+    //carousel
+    $('.j-carousel').slick({
+      dots: true
+    });
 
 });
